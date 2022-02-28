b92a9ee7784e1601e50df44d3b272bcfcf6a2530
awaisanwar544 <awaisanwar544@gmail.com>
2022-03-01 03:10:03 +0500

    Add basic structure

---
+++ src/modules/addScore.js
@@ -0,0 +1,17 @@
+import input from './input.js';
+
+const addScore = () => {
+  const section = document.createElement('section');
+  section.setAttribute('id', 'addScore-container');
+  section.innerHTML = `
+            <h2>Add your Score</h2>
+            ${input('text', 'name', '', 'Your Name', '')}
+            <br>
+            ${input('number', 'score', '', 'Your Score', '')}
+            <br>
+            ${input('button', 'score-btn', 'btn', '', 'Add Score')}
+    `;
+  return section;
+};
+
+export default addScore;
