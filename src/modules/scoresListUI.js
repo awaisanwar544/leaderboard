1c337eeed7370278304d261b3675bb08cb53dac0
awaisanwar544 <awaisanwar544@gmail.com>
2022-03-02 22:04:20 +0500

    add styling

--- src/modules/scoresListUI.js
+++ src/modules/scoresListUI.js
@@ -3,10 +3,13 @@ import input from './input.js';
 const scoresListUI = () => {
   const section = document.createElement('section');
   section.setAttribute('id', 'scoreList-container');
+  section.setAttribute('class', 'g-container');
 
   section.innerHTML = `
-                <h2>Recent Score</h2>
-                ${input('button', 'refresh-btn', 'btn', '', 'Refresh')}
+                <div id="score-head">
+                  <h2>Recent Score</h2>
+                  ${input('button', 'refresh-btn', 'btn', '', 'Refresh')}
+                </div>
                 <div id="scoreList">
                     <ul id="scoreLis-ul">
                         
